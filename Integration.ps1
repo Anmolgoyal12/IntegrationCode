dc41cac21277a155ebd51e5aeca1ee2bfc95302e
Anmol <06anmol6@gmail.com>
2025-01-31 15:15:38 +0530

    New commit

--- Integration.ps1
+++ Integration.ps1
@@ -2,7 +2,7 @@
 $hostname = "localhost"
 $port = "5432"
 $dbname = "procedure_db"
-$username = "anmol_ta"
+$username = "anmol_t"
 
 # Prompt for password securely
 $password = Read-Host -Prompt "Enter PostgreSQL password" -AsSecureString
